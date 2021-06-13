db834f4ff8e02f09d9e0f6395305eba3af6bfb31
Jrandolph011@gmail.com <Jrandolph011@gmail.com>
2021-06-13 09:04:15 -0500

    working on market tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
+const IPFS = require("ipfs-core");
+
 const Market = artifacts.require("Market");
 const Store = artifacts.require("Store");
 
+
+// Test Market.addStore
 contract("Market", async (accounts) => {
   it("Should add a new store and set it's visibility according to what parameters are passed in", async () => {
     let market = await Market.deployed();
@@ -20,6 +24,7 @@ contract("Market", async (accounts) => {
   });
 });
 
+// Test Market.getStores
 contract("Market", async (accounts) => {
   it("Should return one store ipfs and one empty string", async () => {
     let market = await Market.deployed();
@@ -33,4 +38,37 @@ contract("Market", async (accounts) => {
       JSON.stringify(await market.getStores([0, 1]))
     );
   });
+});
+
+async function getAllStores(start, page_size) {
+  let hasMore = true
+  let allValues = []
+  while (hasMore) {
+    let cursor = 
+    let cursor, values = market.getAllStores()
+  }
+}
+
+// Test Market.getAllStores
+contract("Market", async (accounts) => {
+  it("Should iterate through all available stores", async () => {
+    const market = await Market.deployed();
+    const ipfs = await IPFS.create();
+
+    let hashes = []
+    for (let i = 0; i < 20; i++) {
+      let m = {
+        title: `store ${i}`,
+        description: `This is store ${i}`
+      }
+
+      let cid = await ipfs.add(JSON.stringify(m));
+      hashes.push(cid);
+      await market.addStore(cid['path'], true);
+    }
+
+    let cursor = 0;
+    let {cursor, values} = await market.getAllStores(cursor, 10);
+    
+  })
 });
