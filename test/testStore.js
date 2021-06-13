c94ed8304556187492a2245930a0a65f0ca9c155
Jrandolph011@gmail.com <jrandolph011@gmail.com>
2021-06-13 11:59:52 -0500

    checkpoint

--- test/testStore.js
+++ test/testStore.js
@@ -2,3 +2,21 @@ const IPFS = require("ipfs-core");
 
 const Market = artifacts.require("Market");
 const Store = artifacts.require("Store");
+
+
+contract("Market", async (accounts) => {
+    // Test the creation of a store in the market
+
+    let market = Market.deployed();
+
+    it("Should create a new store contract from the deployed market contract", async () => {
+        await market.addStore("asdf", true);
+
+        let storeAddress = await market.stores(0);
+        let store = new Store(storeAddress);
+
+
+
+        assert.equal(store.ipfs_hash(), 'asdf');
+    });
+});
