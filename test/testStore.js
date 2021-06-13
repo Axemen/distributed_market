dfb2a0a711e6c35d69d6744f4529ea565b7325b3
Jonathan Randolph <Jrandolph011@gmail.com>
2021-06-13 12:18:20 -0500

    adding await

--- test/testStore.js
+++ test/testStore.js
@@ -7,13 +7,14 @@ const Store = artifacts.require("Store");
 contract("Market", async (accounts) => {
     // Test the creation of a store in the market
 
-    let market = Market.deployed();
+    let market = await Market.deployed();
 
     it("Should create a new store contract from the deployed market contract", async () => {
         await market.addStore("asdf", true);
 
         let storeAddress = await market.stores(0);
         let store = new Store(storeAddress);
+
 
 
 
